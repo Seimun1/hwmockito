a78f86c4fa58bd1362950d74707fc1f01d8c2540
Dina-72 <amenova1109@yandex.ru>
2023-07-09 20:52:57 +0500

    first commit

---
+++ src/main/java/ru/netology/javaqa/hwmockito/services/FilmManager.java
@@ -0,0 +1,40 @@
+package ru.netology.javaqa.hwmockito.services;
+public class FilmManager {
+    private String[] films = new String[0];
+    private int limit;
+
+    public FilmManager() { // конструктор без параметров с лимитом 5
+        this.limit = 6;
+    }
+
+    public FilmManager(int limit) { // конструктор с параметром,
+                                   // берущий значение лимита для менеджера из параметра конструктора
+        this.limit = limit;
+    }
+    public void add(String film) { // добавление нового фильма
+        String[] tmp = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = film;
+        films = tmp;
+    }
+
+    public String[] findAll() { // вывод всех фильмов в порядке добавления
+        return films;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (films.length < limit) {
+            resultLength = films.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
+        }
+        return tmp;
+    }
+}
---
+++ src/test/java/ru/netology/javaqa/hwmockito/services/FilmManagerTest.java
@@ -0,0 +1,65 @@
+package ru.netology.javaqa.hwmockito.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FilmManagerTest {
+
+    @Test
+    public void arrayFilm() { // пустой массив фильмов
+        FilmManager manager = new FilmManager();
+
+        String[] expected = {};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addOneFilm() { // добавление фильма в массив
+        FilmManager manager = new FilmManager();
+        manager.add("Фильм 1");
+
+        String[] expected = { "Фильм 1" };
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addMoreOneFilm() {
+        FilmManager manager = new FilmManager();
+        manager.add("Фильм 1");
+        manager.add("Фильм 2");
+        manager.add("Фильм 3");
+
+        String[] expected = { "Фильм 1", "Фильм 2", "Фильм 3"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastReverseFilm() { // вывод последних добавленных фильмов
+        FilmManager manager = new FilmManager();
+        manager.add("Фильм 1");
+        manager.add("Фильм 2");
+        manager.add("Фильм 3");
+
+        String[] expected = { "Фильм 3", "Фильм 2", "Фильм 1" };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastReverseFilmAboveLimit() { // вывод последних добавленных фильмов
+        FilmManager manager = new FilmManager();
+        manager.add("Фильм 1");
+        manager.add("Фильм 2");
+        manager.add("Фильм 3");
+        manager.add("Фильм 4");
+        manager.add("Фильм 5");
+        manager.add("Фильм 6");
+
+        String[] expected = { "Фильм 6", "Фильм 5", "Фильм 4", "Фильм 3", "Фильм 2", "Фильм 1" };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
